cd8c0548308dfe6b655d1e664a3e2b3120522d90
jfeinstein10 <j.feinstein10@gmail.com>
2012-11-06 23:24:11 -0500

    oops

---
+++ obj/local/armeabi/objs/dominantcolors/dominantcolors.o.d
@@ -0,0 +1,110 @@
+obj/local/armeabi/objs/dominantcolors/dominantcolors.o: \
+ jni/dominantcolors.c \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/jni.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/time.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/time.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/types.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/stdint.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/_types.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/_types.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/posix_types.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/stddef.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/compiler.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/posix_types.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/types.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/types.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/kernel.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/time.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/siginfo.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm-generic/siginfo.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/android/log.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/android/bitmap.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/stdio.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/stdlib.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/string.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/malloc.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/alloca.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/strings.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/memory.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/math.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/limits.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/limits.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/limits.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/internal_types.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/limits.h \
+ /home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/syslimits.h
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/jni.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/time.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/time.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/types.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/stdint.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/_types.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/_types.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/posix_types.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/stddef.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/compiler.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/posix_types.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/types.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/types.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/kernel.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/time.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/siginfo.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/android/log.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/android/bitmap.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/stdio.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/stdlib.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/string.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/malloc.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/alloca.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/strings.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/memory.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/math.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/limits.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/limits.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/limits.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/internal_types.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/limits.h:
+
+/home/jeremy/android-sdk-linux/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/syslimits.h:
