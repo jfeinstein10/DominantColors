514d03781688b53fa45f33a1fb2cf9204591d58e
jfeinstein10 <j.feinstein10@gmail.com>
2012-11-06 23:41:31 -0500

    Fixed tabbing

--- jni/dominantcolors.c
+++ jni/dominantcolors.c
@@ -50,111 +50,110 @@ static uint32_t color(int r, int g, int b) {
 }
 
 static double distance(uint32_t c1, uint32_t c2) {
-	return sqrt(pow(red(c1)-red(c2),2) + 
-		pow(green(c1)-green(c2),2) + 
-		pow(blue(c1)-blue(c2),2));
+  return sqrt(pow(red(c1)-red(c2),2) + 
+    pow(green(c1)-green(c2),2) + 
+    pow(blue(c1)-blue(c2),2));
 }
 
 static void kmeans(AndroidBitmapInfo* info, void* pixels, int numColors, jint* centroids){
-	int xx, yy, c;
-	uint32_t* start = (uint32_t*)pixels;
-	uint32_t* line;
-	
-	color_sum sums[numColors];
-	int members[numColors];
-	int filled = 0;
-	uint32_t new_color;
-	while (filled < numColors) {
-		xx = rand() % info->width;
-		yy = rand() % info->height;
-		new_color = (uint32_t) ((char *)pixels + yy*info->stride)[xx];
-		bool contained = false;
-		int i;
-		for (i = 0; i < filled; contained |= (centroids[i++] == new_color));
-		if (!contained) { 
-			LOGI("%X\n", new_color);
-			centroids[filled++] = new_color; 
-		}
-	}
+  int xx, yy, c;
+  uint32_t* start = (uint32_t*)pixels;
+  uint32_t* line;
+  
+  color_sum sums[numColors];
+  int members[numColors];
+  int filled = 0;
+  uint32_t new_color;
+  while (filled < numColors) {
+    xx = rand() % info->width;
+	yy = rand() % info->height;
+	new_color = (uint32_t) ((char *)pixels + yy*info->stride)[xx];
+	bool contained = false;
+	int i;
+	for (i = 0; i < filled; contained |= (centroids[i++] == new_color));
+	if (!contained) { 
+	  LOGI("%X\n", new_color);
+	  centroids[filled++] = new_color; 
+    }
+  } 
 
-	double max_error;
-	int index = 0;
+  double max_error;
+  int index = 0;
 		
-	do {
-		max_error = 0;
+  do {
+    max_error = 0;
+	for (c = 0; c < numColors; c++) {
+	  color_sum s = { 0, 0, 0 };
+	  sums[c] = s;
+	  members[c] = 0;
+	}
+	line = start;
+	for (yy = 0; yy < info->height; yy++){
+	  for (xx = 0; xx < info->width; xx++){
+		double min_dist = DBL_MAX;
+		int best_centroid = 0;
 		for (c = 0; c < numColors; c++) {
-			color_sum s = { 0, 0, 0 };
-			sums[c] = s;
-			members[c] = 0;
-		}
-		line = start;
-		for (yy = 0; yy < info->height; yy++){
-			for (xx = 0; xx < info->width; xx++){
-				double min_dist = DBL_MAX;
-				int best_centroid = 0;
-				for (c = 0; c < numColors; c++) {
-					double dist = distance(line[xx], centroids[c]);
-					if (dist < min_dist) {
-						min_dist = dist;
-						best_centroid = c;
-					}
-				}
-				sums[best_centroid].red += red(line[xx]);
-				sums[best_centroid].green += green(line[xx]);
-				sums[best_centroid].blue += blue(line[xx]);
-				members[best_centroid]++;
-	        }
-	        line = (uint32_t*) ((char*)line + info->stride);
-		}
-		for (c = 0; c < numColors; c++) {
-			uint32_t new_centroid;
-			if (members[c] == 0) {
-				new_centroid = 0xFFFFFFFF;
-			} else {
-				new_centroid = color(sums[c].red/members[c], 
-					sums[c].green/members[c],
-					sums[c].blue/members[c]);
+			double dist = distance(line[xx], centroids[c]);
+			if (dist < min_dist) {
+			  min_dist = dist;
+			  best_centroid = c;
 			}
-			double dist = distance(new_centroid, centroids[c]);
-			if (dist > max_error) {
-				max_error = dist;
-			}
-			centroids[c] = new_centroid;
 		}
-	} while (index++ < 100 && max_error > 1);
+		sums[best_centroid].red += red(line[xx]);
+		sums[best_centroid].green += green(line[xx]);
+		sums[best_centroid].blue += blue(line[xx]);
+		members[best_centroid]++;
+	  }
+	  line = (uint32_t*) ((char*)line + info->stride);
+	}
+	for (c = 0; c < numColors; c++) {
+	  uint32_t new_centroid;
+	  if (members[c] == 0) {
+		new_centroid = 0xFFFFFFFF;
+	  } else {
+		new_centroid = color(sums[c].red/members[c], 
+		sums[c].green/members[c],
+		sums[c].blue/members[c]);
+	  }
+	  double dist = distance(new_centroid, centroids[c]);
+	  if (dist > max_error) {
+	    max_error = dist;
+	  }
+	  centroids[c] = new_centroid;
+    }
+  } while (index++ < 100 && max_error > 1);
 }
 
 
 JNIEXPORT jintArray JNICALL Java_com_dominantcolors_DominantColors_kmeans(JNIEnv * env, jobject  obj, jobject bitmap, jint numColors)
 {
-
-    AndroidBitmapInfo info;
-    int ret;
-    void* pixels;
+  AndroidBitmapInfo info;
+  int ret;
+  void* pixels;
 	
-    if ((ret = AndroidBitmap_getInfo(env, bitmap, &info)) < 0) {
-        LOGE("AndroidBitmap_getInfo() failed ! error=%d", ret);
-        return NULL;
-    }
+  if ((ret = AndroidBitmap_getInfo(env, bitmap, &info)) < 0) {
+    LOGE("AndroidBitmap_getInfo() failed ! error=%d", ret);
+    return NULL;
+  }
     
-    if (info.format != ANDROID_BITMAP_FORMAT_RGBA_8888) {
-        LOGE("Bitmap format is not RGBA_8888!");
-        return NULL;
-    }
+  if (info.format != ANDROID_BITMAP_FORMAT_RGBA_8888) {
+    LOGE("Bitmap format is not RGBA_8888!");
+    return NULL;
+  }
 
-    if ((ret = AndroidBitmap_lockPixels(env, bitmap, &pixels)) < 0) {
-        LOGE("AndroidBitmap_lockPixels() failed ! error=%d", ret);
-    }    
+  if ((ret = AndroidBitmap_lockPixels(env, bitmap, &pixels)) < 0) {
+    LOGE("AndroidBitmap_lockPixels() failed ! error=%d", ret);
+  }    
     
-    jintArray result;
-    result = (*env)->NewIntArray(env, numColors);
-    if (result == NULL)
-    	return NULL;
-    jint fill[numColors];
-   	kmeans(&info, pixels, numColors, fill);   
+  jintArray result;
+  result = (*env)->NewIntArray(env, numColors);
+  if (result == NULL)
+  	return NULL;
+  jint fill[numColors];
+  kmeans(&info, pixels, numColors, fill);   
+  
+  (*env)->SetIntArrayRegion(env, result, 0, numColors, fill);
 	
-   	(*env)->SetIntArrayRegion(env, result, 0, numColors, fill);
-	
-    AndroidBitmap_unlockPixels(env, bitmap);
-    return result;
+  AndroidBitmap_unlockPixels(env, bitmap);
+  return result;
 }
